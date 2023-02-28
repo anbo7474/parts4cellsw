9aac673e084181ffa3128b3a95a983701778f15a
anbo7474 <124722753+anbo7474@users.noreply.github.com>
2023-02-28 13:55:10 -0600

    lr
    28.02.2023

--- src/app/content/inicio/inicio.component.ts
+++ src/app/content/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CargarscriptsService } from 'src/app/cargarscripts.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class InicioComponent {
 
-}
+  constructor (private Cargarscripts: CargarscriptsService){
+    Cargarscripts.cargar ([
+      "assets/vendor/purecounter/purecounter_vanilla.js",
+      "assets/vendor/aos/aos.js",
+      "assets/vendor/bootstrap/js/bootstrap.bundle.min.js",
+      "assets/vendor/glightbox/js/glightbox.min.js",
+      "assets/vendor/isotope-layout/isotope.pkgd.min.js",
+      "assets/vendor/swiper/swiper-bundle.min.js",
+      "assets/vendor/waypoints/noframework.waypoints.js",
+      "assets/vendor/php-email-form/validate.js",
+     "assets/js/main.js"
+        ])
+      }
+    }
+
